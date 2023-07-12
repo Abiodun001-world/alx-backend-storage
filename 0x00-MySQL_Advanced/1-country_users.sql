baab72bf0e4c49b676750798d89be150ddc71201
Abiodun001-wold <adekunleabiodun354@gmail.com>
2023-07-12 15:08:45 -0400

    in and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,9 @@
+-- creates a table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT, 
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
 );
 
